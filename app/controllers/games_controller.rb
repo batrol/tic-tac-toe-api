4eb0cdef7921aaf4489dbf87ddc24dd24c5f1832
Plinio Almeida <plinioalmeida20@gmail.com>
2021-05-02 03:05:18 +0200

    Games API
    Implementing controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,8 @@
 class GamesController < ApplicationController
+  include Response
+
+  # POST /games
+  def create
+    json_response([], :created)
+  end
 end
--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Games API', type: :requests do
+RSpec.describe 'Games API', type: :request do
 
   describe 'Players can request a new game' do
     before { post '/games' }
@@ -9,8 +9,8 @@ RSpec.describe 'Games API', type: :requests do
       expect(json).not_to be_empty
     end
 
-    it 'returns status code 200' do
-      expect(response).to have_http_status(200)
+    it 'returns status code 201 (:created)' do
+      expect(response).to have_http_status(201)
     end
   end
 end
